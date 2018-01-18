44416495d3376246b5459820f6fa0ac8f6de01b9
Mariano Achaval <mariano.achaval@mulesoft.com>
2018-01-17 22:59:29 -0300

    Small fixes

--- src/main/java/org/mule/tooling/lang/dw/parser/psi/WeaveNamedElementImpl.java
+++ src/main/java/org/mule/tooling/lang/dw/parser/psi/WeaveNamedElementImpl.java
@@ -9,35 +9,39 @@ import org.mule.tooling.lang.dw.reference.WeaveIdentifierPsiReference;
 
 public abstract class WeaveNamedElementImpl extends ASTWrapperPsiElement implements WeaveNamedElement {
 
-  public WeaveNamedElementImpl(@NotNull ASTNode node) {
-    super(node);
-  }
-
-  public String getName() {
-    return getIdentifier().getName();
-  }
-
-  public PsiElement setName(@NotNull String newName) {
-    ASTNode keyNode = getIdentifier().getNode();
-    if (keyNode != null) {
-      WeaveIdentifier property = WeaveElementFactory.createIdentifier(getProject(), newName);
-      getIdentifier().getParent().getNode().replaceChild(keyNode, property.getNode());
+    public WeaveNamedElementImpl(@NotNull ASTNode node) {
+        super(node);
     }
-    return this;
-  }
 
-  public PsiElement getNameIdentifier() {
-    return getIdentifier();
-  }
-
-  public PsiReference getReference() {
-    if (getNameIdentifier() != null) {
-      return new WeaveIdentifierPsiReference(this);
-    } else {
-      return null;
+    public String getName() {
+        if (getIdentifier() == null) {
+            return null;
+        } else {
+            return getIdentifier().getName();
+        }
     }
-  }
 
-  @NotNull
-  public abstract WeaveIdentifier getIdentifier();
+    public PsiElement setName(@NotNull String newName) {
+        ASTNode keyNode = getIdentifier().getNode();
+        if (keyNode != null) {
+            WeaveIdentifier property = WeaveElementFactory.createIdentifier(getProject(), newName);
+            getIdentifier().getParent().getNode().replaceChild(keyNode, property.getNode());
+        }
+        return this;
+    }
+
+    public PsiElement getNameIdentifier() {
+        return getIdentifier();
+    }
+
+    public PsiReference getReference() {
+        if (getNameIdentifier() != null) {
+            return new WeaveIdentifierPsiReference(this);
+        } else {
+            return null;
+        }
+    }
+
+
+    public abstract WeaveIdentifier getIdentifier();
 }
