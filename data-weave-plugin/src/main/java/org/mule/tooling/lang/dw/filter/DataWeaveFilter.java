91818f20c24878afe9e5a04fddc28215ba8a2822
mariano.deachaval <mariano.achaval@mulesoft.com>
2018-05-03 12:36:01 -0300

    Fix location

--- data-weave-plugin/src/main/java/org/mule/tooling/lang/dw/filter/DataWeaveFilter.java
+++ data-weave-plugin/src/main/java/org/mule/tooling/lang/dw/filter/DataWeaveFilter.java
@@ -5,9 +5,6 @@ import com.intellij.execution.filters.HyperlinkInfo;
 import com.intellij.execution.filters.OpenFileHyperlinkInfo;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.search.FilenameIndex;
-import com.intellij.psi.search.GlobalSearchScope;
 import org.jetbrains.annotations.Nullable;
 import org.mule.tooling.lang.dw.util.VirtualFileSystemUtils;
 import org.mule.weave.v2.parser.ast.variables.NameIdentifier;
@@ -21,7 +18,7 @@ public class DataWeaveFilter implements Filter {
 
     private Project myProject;
 
-    private Pattern ELEMENT_REGEX = Pattern.compile("(?<nameIdentifier>[A-z0-9_:]*)(\\s\\(line:\\s(?<lineNumber>[0-9]+), column:\\s(?<columnNumber>[0-9]+)\\))");
+    private Pattern ELEMENT_REGEX = Pattern.compile("(?<nameIdentifier>[A-z0-9_:]*)(\\s*\\(line:\\s*(?<lineNumber>[0-9]+), column:\\s*(?<columnNumber>[0-9]+)\\))");
 
     public DataWeaveFilter(Project myProject) {
         this.myProject = myProject;
@@ -32,7 +29,6 @@ public class DataWeaveFilter implements Filter {
     public Result applyFilter(String textLine, int entireLength) {
         final Matcher matcher = ELEMENT_REGEX.matcher(textLine);
         if (matcher.find()) {
-            String element = matcher.group(0);
             final String nameIdentifierText = matcher.group("nameIdentifier");
             final String lineNumber = matcher.group("lineNumber");
             final String columnNumber = matcher.group("columnNumber");
@@ -47,8 +43,10 @@ public class DataWeaveFilter implements Filter {
             if (virtualFile == null) {
                 return null;
             } else {
+                int startLocation = entireLength - textLine.length() + matcher.start();
+                int endLocation = entireLength - textLine.length() + matcher.end();
                 HyperlinkInfo info = new OpenFileHyperlinkInfo(myProject, virtualFile, Integer.parseInt(lineNumber) - 1, Integer.parseInt(columnNumber));
-                return new Result(entireLength - element.length(), entireLength, info);
+                return new Result(startLocation, endLocation, info);
             }
         }
         return null;
