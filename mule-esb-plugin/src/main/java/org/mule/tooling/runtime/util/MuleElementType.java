0c98ef0bafd3901fae67d027533d167efded2758
mariano.deachaval <mariano.achaval@mulesoft.com>
2018-05-28 15:52:22 -0300

    Smart connectors in progress

---
+++ mule-esb-plugin/src/main/java/org/mule/tooling/runtime/util/MuleElementType.java
@@ -0,0 +1,21 @@
+package org.mule.tooling.runtime.util;
+
+import java.util.Arrays;
+
+public enum MuleElementType {
+    MESSAGE_SOURCE("abstractMessageSourceType", "inboundEndpointType"),
+    MESSAGE_PROCESSOR("abstractMessageProcessorType", "outboundEndpointType"),
+    EXCEPTION_STRATEGY("abstractExceptionStrategyType"),
+    CONFIG("abstractExtensionType"),
+    TRANSPORT_CONNECTOR("abstractConnectorType");
+
+    private String[] validTypes;
+
+    MuleElementType(String... validTypes) {
+        this.validTypes = validTypes;
+    }
+
+    public boolean isValidType(String type) {
+        return Arrays.asList(validTypes).contains(type);
+    }
+}
---
+++ mule-esb-plugin/src/main/java/org/mule/tooling/runtime/wizard/SmartConnectorModuleBuilder.java
@@ -0,0 +1,7 @@
+package org.mule.tooling.runtime.wizard;
+
+import com.intellij.ide.util.projectWizard.SourcePathsBuilder;
+import org.jetbrains.idea.maven.wizards.MavenModuleBuilder;
+
+public class SmartConnectorModuleBuilder extends MavenModuleBuilder implements SourcePathsBuilder {
+}
