f0f622cdaea6b031be9d558af6d5adee5b78a90f
estebanwasinger <estebanwasinger@gmail.com>
2018-06-24 00:59:26 -0300

    Use always Maven Central remote repository

--- mule-esb-plugin/src/main/java/org/mule/tooling/runtime/util/MavenConfigurationUtils.java
+++ mule-esb-plugin/src/main/java/org/mule/tooling/runtime/util/MavenConfigurationUtils.java
@@ -1,8 +1,11 @@
 package org.mule.tooling.runtime.util;
 
 import org.mule.maven.client.api.model.MavenConfiguration;
+import org.mule.maven.client.api.model.RemoteRepository;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Optional;
 
 public class MavenConfigurationUtils {
@@ -12,8 +15,22 @@ public class MavenConfigurationUtils {
     final MavenConfiguration.MavenConfigurationBuilder mavenConfigurationBuilder = MavenConfiguration.newMavenConfigurationBuilder();
     mavenConfigurationBuilder.forcePolicyUpdateNever(false);
     mavenConfigurationBuilder.localMavenRepositoryLocation(MuleDirectoriesUtils.getMavenTooling());
+    addMavenCentralRemoteRepository(mavenConfigurationBuilder);
     userSettingsLocation.ifPresent(mavenConfigurationBuilder::userSettingsLocation);
     globalSettingsLocation.ifPresent(mavenConfigurationBuilder::globalSettingsLocation);
     return mavenConfigurationBuilder.build();
+  }
+
+  private static void addMavenCentralRemoteRepository(MavenConfiguration.MavenConfigurationBuilder mavenConfigurationBuilder) {
+    try {
+        mavenConfigurationBuilder.remoteRepository(RemoteRepository
+                .newRemoteRepositoryBuilder()
+                .id("maven-central")
+                .url(new URL("https://repo.maven.apache.org/maven2/"))
+                .build());
+    } catch (MalformedURLException e) {
+      //This should never happen
+      throw new RuntimeException(e);
+    }
   }
 }
