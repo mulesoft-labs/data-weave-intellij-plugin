28ef269cb98b79dcc20af407d6421b2afdf11b2b
mariano.deachaval <mariano.achaval@mulesoft.com>
2018-07-13 19:09:32 -0300

    Fix VirtualFileSystemApi to new version

--- data-weave-plugin/src/main/java/org/mule/tooling/lang/dw/service/IJVirtualFileSystemAdaptor.java
+++ data-weave-plugin/src/main/java/org/mule/tooling/lang/dw/service/IJVirtualFileSystemAdaptor.java
@@ -30,156 +30,157 @@ import java.util.List;
 
 public class IJVirtualFileSystemAdaptor implements VirtualFileSystem, Disposable {
 
-    private Project project;
+  private Project project;
 
-    private List<ChangeListener> listeners;
+  private List<ChangeListener> listeners;
 
   public IJVirtualFileSystemAdaptor(Project project) {
-        this.project = project;
-        this.listeners = new ArrayList<>();
+    this.project = project;
+    this.listeners = new ArrayList<>();
 
-        PsiManager.getInstance(this.project).addPsiTreeChangeListener(new PsiTreeAnyChangeAbstractAdapter() {
-            @Override
-            protected void onChange(@Nullable PsiFile file) {
-                if (file != null) {
-                    VirtualFile virtualFile = file.getVirtualFile();
-                    onFileChanged(virtualFile);
-                }
-            }
-        });
-
-        VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileListener() {
-            @Override
-            public void contentsChanged(@NotNull VirtualFileEvent event) {
-                onFileChanged(event.getFile());
-            }
-        });
-    }
-
-    @Override
-    public void removeChangeListener(ChangeListener listener) {
-        this.listeners.remove(listener);
-    }
-
-    @Override
-    public org.mule.weave.v2.editor.VirtualFile file(String path) {
-        final VirtualFile fileByUrl = VirtualFileManager.getInstance().findFileByUrl(path);
-        if (fileByUrl == null) {
-            return null;
-        } else {
-          return new IJVirtualFileAdaptor(this, fileByUrl, this.project, null);
+    PsiManager.getInstance(this.project).addPsiTreeChangeListener(new PsiTreeAnyChangeAbstractAdapter() {
+      @Override
+      protected void onChange(@Nullable PsiFile file) {
+        if (file != null) {
+          VirtualFile virtualFile = file.getVirtualFile();
+          onFileChanged(virtualFile);
         }
+      }
+    });
 
+    VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileListener() {
+      @Override
+      public void contentsChanged(@NotNull VirtualFileEvent event) {
+        onFileChanged(event.getFile());
+      }
+    });
+  }
+
+  @Override
+  public void removeChangeListener(ChangeListener listener) {
+    this.listeners.remove(listener);
+  }
+
+  @Override
+  public org.mule.weave.v2.editor.VirtualFile file(String path) {
+    final VirtualFile fileByUrl = VirtualFileManager.getInstance().findFileByUrl(path);
+    if (fileByUrl == null) {
+      return null;
+    } else {
+      return new IJVirtualFileAdaptor(this, fileByUrl, this.project, null);
+    }
+
+  }
+
+  @Override
+  public void changeListener(ChangeListener cl) {
+    this.listeners.add(cl);
+  }
+
+  @Override
+  public void onChanged(org.mule.weave.v2.editor.VirtualFile vf) {
+    for (ChangeListener listener: listeners) {
+      listener.onChanged(vf);
+    }
+  }
+
+  private void onFileChanged(VirtualFile virtualFile) {
+    ApplicationManager.getApplication().runReadAction(() -> {
+      if (project.isDisposed()) {
+        return;
+      }
+      final VirtualFile contentRootForFile = ProjectFileIndex.SERVICE.getInstance(project).getSourceRootForFile(virtualFile);
+      if (contentRootForFile != null) {
+        //If it is a file from the project
+        final IJVirtualFileAdaptor intellijVirtualFile = new IJVirtualFileAdaptor(IJVirtualFileSystemAdaptor.this, virtualFile, project, null);
+        onChanged(intellijVirtualFile);
+      }
+    });
+  }
+
+  @Override
+  public WeaveResourceResolver asResourceResolver() {
+    return new IntellijWeaveResourceResolver(project, this);
+  }
+
+
+  @Override
+  public void dispose() {
+
+  }
+
+  public static class IntellijWeaveResourceResolver implements WeaveResourceResolver {
+
+    private Project project;
+    private VirtualFileSystem fs;
+
+    public IntellijWeaveResourceResolver(Project project, VirtualFileSystem fs) {
+      this.project = project;
+      this.fs = fs;
     }
 
     @Override
-    public void changeListener(ChangeListener cl) {
-        this.listeners.add(cl);
+    public Option<WeaveResource> resolve(NameIdentifier name) {
+      VirtualFile resolve = VirtualFileSystemUtils.resolve(project, name);
+      if (resolve == null) {
+        return Option.empty();
+      } else {
+        IJVirtualFileAdaptor intellijVirtualFileAdaptor = new IJVirtualFileAdaptor(this.fs, resolve, project, name);
+        return Option.apply(intellijVirtualFileAdaptor.asResource());
+      }
     }
-
-    private void onFileChanged(VirtualFile virtualFile) {
-        ApplicationManager.getApplication().runReadAction(() -> {
-            if (project.isDisposed()) {
-                return;
-            }
-            final VirtualFile contentRootForFile = ProjectFileIndex.SERVICE.getInstance(project).getSourceRootForFile(virtualFile);
-            if (contentRootForFile != null) {
-                //If it is a file from the project
-              final IJVirtualFileAdaptor intellijVirtualFile = new IJVirtualFileAdaptor(IJVirtualFileSystemAdaptor.this, virtualFile, project, null);
-                for (ChangeListener listener : listeners) {
-                    listener.onChanged(intellijVirtualFile);
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onChanged(String path) {
-        //nothing to do here
-    }
-
-    @Override
-    public WeaveResourceResolver asResourceResolver() {
-        return new IntellijWeaveResourceResolver(project, this);
-    }
-
-    @Override
-    public void dispose() {
-
-    }
-
-    public static class IntellijWeaveResourceResolver implements WeaveResourceResolver {
-
-        private Project project;
-        private VirtualFileSystem fs;
-
-        public IntellijWeaveResourceResolver(Project project, VirtualFileSystem fs) {
-            this.project = project;
-            this.fs = fs;
-        }
-
-        @Override
-        public Option<WeaveResource> resolve(NameIdentifier name) {
-          VirtualFile resolve = VirtualFileSystemUtils.resolve(project, name);
-            if (resolve == null) {
-                return Option.empty();
-            } else {
-              IJVirtualFileAdaptor intellijVirtualFileAdaptor = new IJVirtualFileAdaptor(this.fs, resolve, project, name);
-                return Option.apply(intellijVirtualFileAdaptor.asResource());
-            }
-        }
-    }
+  }
 
   public static class IJVirtualFileAdaptor implements org.mule.weave.v2.editor.VirtualFile {
 
-        private VirtualFileSystem fs;
-        private VirtualFile vfs;
-        private Document document;
-        private Project project;
-        private NameIdentifier name;
+    private VirtualFileSystem fs;
+    private VirtualFile vfs;
+    private Document document;
+    private Project project;
+    private NameIdentifier name;
 
     public IJVirtualFileAdaptor(VirtualFileSystem fs, @NotNull VirtualFile vfs, @NotNull Project project, NameIdentifier name) {
-            this.fs = fs;
-            this.vfs = vfs;
-            this.document = ReadAction.compute(() -> FileDocumentManager.getInstance().getDocument(vfs));
-            this.project = project;
-            this.name = name;
-        }
-
-        @Override
-        public VirtualFileSystem fs() {
-            return fs;
-        }
-
-        @Override
-        public String read() {
-            return ReadAction.compute(() -> document.getText());
-        }
-
-        @Override
-        public void write(String content) {
-            if (document != null) {
-                WriteAction.run(() -> document.setText(content));
-            }
-        }
-
-        @Override
-        public boolean readOnly() {
-            return false;
-        }
-
-        @Override
-        public String path() {
-            return vfs.getUrl();
-        }
-
-        @Override
-        public NameIdentifier getNameIdentifier() {
-            if (this.name == null) {
-                this.name = VirtualFileSystemUtils.calculateNameIdentifier(project, vfs);
-            }
-            return this.name;
-        }
-
+      this.fs = fs;
+      this.vfs = vfs;
+      this.document = ReadAction.compute(() -> FileDocumentManager.getInstance().getDocument(vfs));
+      this.project = project;
+      this.name = name;
     }
+
+    @Override
+    public VirtualFileSystem fs() {
+      return fs;
+    }
+
+    @Override
+    public String read() {
+      return ReadAction.compute(() -> document.getText());
+    }
+
+    @Override
+    public void write(String content) {
+      if (document != null) {
+        WriteAction.run(() -> document.setText(content));
+      }
+    }
+
+    @Override
+    public boolean readOnly() {
+      return false;
+    }
+
+    @Override
+    public String path() {
+      return vfs.getUrl();
+    }
+
+    @Override
+    public NameIdentifier getNameIdentifier() {
+      if (this.name == null) {
+        this.name = VirtualFileSystemUtils.calculateNameIdentifier(project, vfs);
+      }
+      return this.name;
+    }
+
+  }
 }
