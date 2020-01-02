a959b2224719841d8d597419e63bea067030feda
afelisatti <afelisatti@mulesoft.com>
2020-01-02 17:04:42 -0300

    DW-80: Add input id

--- data-weave-plugin/src/main/gen/org/mule/tooling/lang/dw/parser/psi/impl/WeaveInputDirectiveImpl.java
+++ data-weave-plugin/src/main/gen/org/mule/tooling/lang/dw/parser/psi/impl/WeaveInputDirectiveImpl.java
@@ -1,14 +1,14 @@
 // This is a generated file. Not intended for manual editing.
 package org.mule.tooling.lang.dw.parser.psi.impl;
 
-import java.util.List;
-import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
-import static org.mule.tooling.lang.dw.parser.psi.WeaveTypes.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.mule.tooling.lang.dw.parser.psi.*;
+
+import java.util.List;
 
 public class WeaveInputDirectiveImpl extends WeaveDirectiveImpl implements WeaveInputDirective {
 
@@ -32,9 +32,9 @@ public class WeaveInputDirectiveImpl extends WeaveDirectiveImpl implements Weave
   }
 
   @Override
-  @Nullable
-  public WeaveIdentifier getIdentifier() {
-    return findChildByClass(WeaveIdentifier.class);
+  @NotNull
+  public List<WeaveIdentifier> getIdentifierList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, WeaveIdentifier.class);
   }
 
   @Override
