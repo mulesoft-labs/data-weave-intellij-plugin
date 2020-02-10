681c96e6388a587dad6309467a0231210c23db7a
machaval <mariano.achaval@mulesoft.com>
2020-02-10 11:03:06 -0300

    Fix parsing issue and structure view not showing annotated functions

--- data-weave-plugin/src/main/gen/org/mule/tooling/lang/dw/parser/WeaveParser.java
+++ data-weave-plugin/src/main/gen/org/mule/tooling/lang/dw/parser/WeaveParser.java
@@ -6,7 +6,6 @@ import com.intellij.lang.PsiBuilder.Marker;
 import static org.mule.tooling.lang.dw.parser.psi.WeaveTypes.*;
 import static org.mule.tooling.lang.dw.parser.WeaveParserUtil.*;
 import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.tree.IFileElementType;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.lang.PsiParser;
@@ -24,16 +23,15 @@ public class WeaveParser implements PsiParser, LightPsiParser {
     boolean r;
     b = adapt_builder_(t, b, this, EXTENDS_SETS_);
     Marker m = enter_section_(b, 0, _COLLAPSE_, null);
-    if (t instanceof IFileElementType) {
-      r = parse_root_(t, b, 0);
-    }
-    else {
-      r = false;
-    }
+    r = parse_root_(t, b);
     exit_section_(b, 0, m, t, r, true, TRUE_CONDITION);
   }
 
-  protected boolean parse_root_(IElementType t, PsiBuilder b, int l) {
+  protected boolean parse_root_(IElementType t, PsiBuilder b) {
+    return parse_root_(t, b, 0);
+  }
+
+  static boolean parse_root_(IElementType t, PsiBuilder b, int l) {
     return root(b, l + 1);
   }
 
