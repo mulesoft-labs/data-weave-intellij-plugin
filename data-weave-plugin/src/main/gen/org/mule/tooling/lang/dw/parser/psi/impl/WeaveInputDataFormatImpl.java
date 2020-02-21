ce6371318c1367deca6da0ddb561930fed196dda
machaval <mariano.achaval@mulesoft.com>
2020-02-21 17:38:06 -0300

    Fix input directives where not working for reference and rename

---
+++ data-weave-plugin/src/main/gen/org/mule/tooling/lang/dw/parser/psi/impl/WeaveInputDataFormatImpl.java
@@ -0,0 +1,41 @@
+// This is a generated file. Not intended for manual editing.
+package org.mule.tooling.lang.dw.parser.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static org.mule.tooling.lang.dw.parser.psi.WeaveTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import org.mule.tooling.lang.dw.parser.psi.*;
+
+public class WeaveInputDataFormatImpl extends ASTWrapperPsiElement implements WeaveInputDataFormat {
+
+  public WeaveInputDataFormatImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull WeaveVisitor visitor) {
+    visitor.visitInputDataFormat(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof WeaveVisitor) accept((WeaveVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @Nullable
+  public WeaveDataFormat getDataFormat() {
+    return findChildByClass(WeaveDataFormat.class);
+  }
+
+  @Override
+  @Nullable
+  public WeaveIdentifier getIdentifier() {
+    return findChildByClass(WeaveIdentifier.class);
+  }
+
+}
