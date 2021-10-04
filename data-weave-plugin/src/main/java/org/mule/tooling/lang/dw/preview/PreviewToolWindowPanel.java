aa89d6aae435b9e35255697fc19028a1ba5c39d6
Mariano De Achaval <mdeachaval@salesforce.com>
2021-10-04 14:25:57 -0300

    Fixing read access violation

--- data-weave-plugin/src/main/java/org/mule/tooling/lang/dw/preview/PreviewToolWindowPanel.java
+++ data-weave-plugin/src/main/java/org/mule/tooling/lang/dw/preview/PreviewToolWindowPanel.java
@@ -2,6 +2,7 @@ package org.mule.tooling.lang.dw.preview;
 
 import com.intellij.ProjectTopics;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent;
 import com.intellij.openapi.fileEditor.FileEditorManagerListener;
@@ -29,142 +30,142 @@ import java.awt.*;
 import java.util.concurrent.ExecutorService;
 
 public class PreviewToolWindowPanel extends SimpleToolWindowPanel implements Disposable {
-    public static final String NOTHING_TO_SHOW = "NOTHING_TO_SHOW";
-    public static final String PREVIEW_EDITOR = "PREVIEW_EDITOR";
-    public static final String NO_RUNTIME_AVAILABLE = "NO_RUNTIME_AVAILABLE";
-    //TODO we should put this in a settings file
+  public static final String NOTHING_TO_SHOW = "NOTHING_TO_SHOW";
+  public static final String PREVIEW_EDITOR = "PREVIEW_EDITOR";
+  public static final String NO_RUNTIME_AVAILABLE = "NO_RUNTIME_AVAILABLE";
+  //TODO we should put this in a settings file
 
-    private @NotNull ExecutorService dependenciesChanges = AppExecutorUtil.createBoundedApplicationPoolExecutor("RootChanges", 15);
+  private @NotNull ExecutorService dependenciesChanges = AppExecutorUtil.createBoundedApplicationPoolExecutor("RootChanges", 15);
 
-    private Project myProject;
-    private WeavePreviewComponent weavePreviewComponent;
-    private JPanel mainPanel;
-    private CardLayout cardLayout;
-    private JComponent previewComponent;
+  private Project myProject;
+  private WeavePreviewComponent weavePreviewComponent;
+  private JPanel mainPanel;
+  private CardLayout cardLayout;
+  private JComponent previewComponent;
 
-    private WeaveAgentService agentRuntimeManager;
+  private WeaveAgentService agentRuntimeManager;
 
-    public PreviewToolWindowPanel(Project project) {
-        super(false);
-        this.myProject = project;
-        this.agentRuntimeManager = WeaveAgentService.getInstance(myProject);
-        cardLayout = new CardLayout();
-        mainPanel = new JPanel(cardLayout);
-        mainPanel.add(new MessagePanel("No DataWeave runtime found."), NO_RUNTIME_AVAILABLE);
-        mainPanel.add(new MessagePanel("No live view available."), NOTHING_TO_SHOW);
-        weavePreviewComponent = new WeavePreviewComponent(project);
-        DumbService.getInstance(myProject).smartInvokeLater(() -> {
-            setupUI(project);
-            initFileListener();
-        });
+  public PreviewToolWindowPanel(Project project) {
+    super(false);
+    this.myProject = project;
+    this.agentRuntimeManager = WeaveAgentService.getInstance(myProject);
+    cardLayout = new CardLayout();
+    mainPanel = new JPanel(cardLayout);
+    mainPanel.add(new MessagePanel("No DataWeave runtime found."), NO_RUNTIME_AVAILABLE);
+    mainPanel.add(new MessagePanel("No live view available."), NOTHING_TO_SHOW);
+    weavePreviewComponent = new WeavePreviewComponent(project);
+    DumbService.getInstance(myProject).smartInvokeLater(() -> {
+      setupUI(project);
+      initFileListener();
+    });
+  }
+
+  private void setupUI(Project project) {
+    previewComponent = weavePreviewComponent.createComponent();
+    mainPanel.add(previewComponent, PREVIEW_EDITOR);
+    cardLayout.show(mainPanel, NOTHING_TO_SHOW);
+    setContent(mainPanel);
+  }
+
+  private void initFileListener() {
+    setFile(getSelectedPsiFile());
+    final MessageBusConnection[] connection = {null};
+
+    myProject.getMessageBus().connect(this).subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
+
+      @Override
+      public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
+        if (!weavePreviewComponent.isPinned()) {
+          final PsiFile psiFile = file.isValid() ? PsiManager.getInstance(myProject).findFile(file) : null;
+          // This invokeLater is required. The problem is open does a commit to PSI, but open is
+          // invoked inside PSI change event. It causes an Exception like "Changes to PSI are not allowed inside event processing"
+          DumbService.getInstance(myProject).smartInvokeLater(() -> setFile(psiFile));
+        }
+      }
+
+      @Override
+      public void selectionChanged(@NotNull FileEditorManagerEvent e) {
+
+        if (agentRuntimeManager.isWeaveRuntimeInstalled()) {
+          if (connection[0] != null) {
+            connection[0].disconnect();
+            connection[0] = null;
+          }
+          showFile(e);
+        } else {
+          cardLayout.show(mainPanel, NO_RUNTIME_AVAILABLE);
+
+          //Once agent is available we need to reset the UI.
+          if (connection[0] != null) {
+            connection[0].disconnect();
+            connection[0] = null;
+          }
+
+          connection[0] = myProject.getMessageBus().connect(myProject);
+          connection[0].subscribe(ProjectTopics.PROJECT_ROOTS, new ModuleRootListener() {
+            @Override
+            public void rootsChanged(ModuleRootEvent event) {
+              dependenciesChanges.submit(() -> {
+                if (agentRuntimeManager.isWeaveRuntimeInstalled()) {
+                  showFile(e);
+                  connection[0].disconnect();
+                  connection[0] = null;
+                }
+              });
+
+            }
+          });
+        }
+      }
+    });
+  }
+
+  private void showFile(@NotNull FileEditorManagerEvent e) {
+    if (!weavePreviewComponent.isPinned()) {
+      VirtualFile file = e.getNewFile();
+      final PsiFile psiFile = ReadAction.compute(() -> file != null && file.isValid() ? PsiManager.getInstance(myProject).findFile(file) : null);
+      // This invokeLater is required. The problem is open does a commit to PSI, but open is
+      // invoked inside PSI change event. It causes an Exception like "Changes to PSI are not allowed inside event processing"
+      DumbService.getInstance(myProject).smartInvokeLater(() -> setFile(psiFile));
     }
+  }
 
-    private void setupUI(Project project) {
+  @Nullable
+  private PsiFile getSelectedPsiFile() {
+    VirtualFile[] files = FileEditorManager.getInstance(myProject).getSelectedFiles();
+    return files.length == 0 ? null : PsiManager.getInstance(myProject).findFile(files[0]);
+  }
+
+
+  private void setFile(@Nullable PsiFile psiFile) {
+    if (psiFile == null) {
+      cardLayout.show(mainPanel, NOTHING_TO_SHOW);
+    } else if (psiFile != weavePreviewComponent.getCurrentFile() && psiFile.getFileType() == WeaveFileType.getInstance()) {
+      WeaveDocument weaveDocument = WeavePsiUtils.getWeaveDocument(psiFile);
+      if (weaveDocument != null && weaveDocument.isMappingDocument()) {
+        if (previewComponent != null) {
+          weavePreviewComponent.dispose();
+          mainPanel.remove(previewComponent);
+          previewComponent = null;
+        }
         previewComponent = weavePreviewComponent.createComponent();
         mainPanel.add(previewComponent, PREVIEW_EDITOR);
-        cardLayout.show(mainPanel, NOTHING_TO_SHOW);
-        setContent(mainPanel);
+        weavePreviewComponent.open(psiFile);
+        cardLayout.show(mainPanel, PREVIEW_EDITOR);
+      }
     }
+  }
 
-    private void initFileListener() {
-        setFile(getSelectedPsiFile());
-        final MessageBusConnection[] connection = {null};
 
-        myProject.getMessageBus().connect(this).subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
-
-            @Override
-            public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
-                if (!weavePreviewComponent.isPinned()) {
-                    final PsiFile psiFile = file.isValid() ? PsiManager.getInstance(myProject).findFile(file) : null;
-                    // This invokeLater is required. The problem is open does a commit to PSI, but open is
-                    // invoked inside PSI change event. It causes an Exception like "Changes to PSI are not allowed inside event processing"
-                    DumbService.getInstance(myProject).smartInvokeLater(() -> setFile(psiFile));
-                }
-            }
-
-            @Override
-            public void selectionChanged(@NotNull FileEditorManagerEvent e) {
-
-                if (agentRuntimeManager.isWeaveRuntimeInstalled()) {
-                    if (connection[0] != null) {
-                        connection[0].disconnect();
-                        connection[0] = null;
-                    }
-                    showFile(e);
-                } else {
-                    cardLayout.show(mainPanel, NO_RUNTIME_AVAILABLE);
-
-                    //Once agent is available we need to reset the UI.
-                    if (connection[0] != null) {
-                        connection[0].disconnect();
-                        connection[0] = null;
-                    }
-
-                    connection[0] = myProject.getMessageBus().connect(myProject);
-                    connection[0].subscribe(ProjectTopics.PROJECT_ROOTS, new ModuleRootListener() {
-                        @Override
-                        public void rootsChanged(ModuleRootEvent event) {
-                            dependenciesChanges.submit(() -> {
-                                if (agentRuntimeManager.isWeaveRuntimeInstalled()) {
-                                    showFile(e);
-                                    connection[0].disconnect();
-                                    connection[0] = null;
-                                }
-                            });
-
-                        }
-                    });
-                }
-            }
-        });
+  @Override
+  public void dispose() {
+    if (weavePreviewComponent != null) {
+      Disposer.dispose(weavePreviewComponent);
+      weavePreviewComponent = null;
     }
+  }
 
-    private void showFile(@NotNull FileEditorManagerEvent e) {
-        if (!weavePreviewComponent.isPinned()) {
-            VirtualFile file = e.getNewFile();
-            final PsiFile psiFile = file != null && file.isValid() ? PsiManager.getInstance(myProject).findFile(file) : null;
-            // This invokeLater is required. The problem is open does a commit to PSI, but open is
-            // invoked inside PSI change event. It causes an Exception like "Changes to PSI are not allowed inside event processing"
-            DumbService.getInstance(myProject).smartInvokeLater(() -> setFile(psiFile));
-        }
-    }
-
-    @Nullable
-    private PsiFile getSelectedPsiFile() {
-        VirtualFile[] files = FileEditorManager.getInstance(myProject).getSelectedFiles();
-        return files.length == 0 ? null : PsiManager.getInstance(myProject).findFile(files[0]);
-    }
-
-
-    private void setFile(@Nullable PsiFile psiFile) {
-        if (psiFile == null) {
-            cardLayout.show(mainPanel, NOTHING_TO_SHOW);
-        } else if (psiFile != weavePreviewComponent.getCurrentFile() && psiFile.getFileType() == WeaveFileType.getInstance()) {
-            WeaveDocument weaveDocument = WeavePsiUtils.getWeaveDocument(psiFile);
-            if (weaveDocument != null && weaveDocument.isMappingDocument()) {
-                if (previewComponent != null) {
-                    weavePreviewComponent.dispose();
-                    mainPanel.remove(previewComponent);
-                    previewComponent = null;
-                }
-                previewComponent = weavePreviewComponent.createComponent();
-                mainPanel.add(previewComponent, PREVIEW_EDITOR);
-                weavePreviewComponent.open(psiFile);
-                cardLayout.show(mainPanel, PREVIEW_EDITOR);
-            }
-        }
-    }
-
-
-    @Override
-    public void dispose() {
-        if (weavePreviewComponent != null) {
-            Disposer.dispose(weavePreviewComponent);
-            weavePreviewComponent = null;
-        }
-    }
-
-    public void setNameChanger(PreviewToolWindowFactory.NameChanger nameChanger) {
-        weavePreviewComponent.setNameChanger(nameChanger);
-    }
+  public void setNameChanger(PreviewToolWindowFactory.NameChanger nameChanger) {
+    weavePreviewComponent.setNameChanger(nameChanger);
+  }
 }
