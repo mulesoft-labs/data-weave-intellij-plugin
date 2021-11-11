f2e3ce9538aeb8463f004a56792e075f7d76dc9d
Mariano De Achaval <mdeachaval@salesforce.com>
2021-11-11 18:37:16 -0300

    Adding simple validation for valid file paths

---
+++ data-weave-plugin/src/main/java/org/mule/tooling/restsdk/annotator/RestSdkAnnotator.java
@@ -0,0 +1,35 @@
+package org.mule.tooling.restsdk.annotator;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.yaml.psi.YAMLKeyValue;
+import org.jetbrains.yaml.psi.YAMLScalar;
+import org.mule.tooling.restsdk.utils.RestSdkHelper;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RestSdkAnnotator implements Annotator {
+  private List<String> URL_KEY_NAMES = Arrays.asList("url", "typeSchema", "outputType");
+
+  @Override
+  public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+    if (RestSdkHelper.isRestSdkDescriptorFile(element.getContainingFile())) {
+      if (element instanceof YAMLScalar) {
+        PsiElement parent = element.getParent();
+        if (parent instanceof YAMLKeyValue && URL_KEY_NAMES.contains(((YAMLKeyValue) parent).getKeyText())) {
+          VirtualFile virtualFile = element.getContainingFile().getVirtualFile();
+          String path = ((YAMLScalar) element).getTextValue();
+          final VirtualFile child = virtualFile.getParent().findFileByRelativePath(path);
+          if (child == null) {
+            holder.newAnnotation(HighlightSeverity.ERROR, "Unable to resolve path to : " + path).range(element).create();
+          }
+        }
+      }
+    }
+  }
+}
