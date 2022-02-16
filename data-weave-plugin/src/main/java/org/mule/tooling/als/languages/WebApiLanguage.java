af9b490df4a42028cf1e5bd583ae68ac329f45ad
Mariano De Achaval <mariano.achaval@mulesoft.com>
2022-02-16 15:59:40 -0300

    Adding asyncapi support

--- data-weave-plugin/src/main/java/org/mule/tooling/als/languages/WebApiLanguage.java
+++ data-weave-plugin/src/main/java/org/mule/tooling/als/languages/WebApiLanguage.java
@@ -14,16 +14,20 @@ public class WebApiLanguage implements ALSLanguageExtension {
 
   public static SelectionPath swaggerVersion = SelectionPath.DOCUMENT.child("swagger");
   public static SelectionPath openApiVersion = SelectionPath.DOCUMENT.child("openapi");
+  public static SelectionPath asyncApiVersion = SelectionPath.DOCUMENT.child("asyncapi");
 
   @Override
   public boolean supports(PsiFile psiFile) {
     if (psiFile.getFileType() instanceof YAMLFileType) {
-      return (swaggerVersion.selectYaml(psiFile) != null) ||
+      return swaggerVersion.selectYaml(psiFile) != null ||
               openApiVersion.selectYaml(psiFile) != null ||
+              asyncApiVersion.selectYaml(psiFile) != null ||
               Objects.equals(psiFile.getOriginalFile().getVirtualFile().getExtension(), "raml");
 
     } else if (psiFile.getFileType() instanceof JsonFileType) {
-      return (swaggerVersion.selectJson(psiFile) != null) || openApiVersion.selectJson(psiFile) != null;
+      return swaggerVersion.selectJson(psiFile) != null ||
+              openApiVersion.selectJson(psiFile) != null ||
+              asyncApiVersion.selectJson(psiFile) != null;
     } else {
       return false;
     }
