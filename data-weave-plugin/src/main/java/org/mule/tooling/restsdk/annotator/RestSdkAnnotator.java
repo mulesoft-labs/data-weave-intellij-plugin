43d637d26b2526d59aa4ca6c645e96a061b2fa22
Nicolás Lichtmaier <nlichtmaier@mulesoft.com>
2022-09-29 14:41:31 -0300

    Show error when trying to define an endpoint operation using a method that the spec doesn't use.

--- data-weave-plugin/src/main/java/org/mule/tooling/restsdk/annotator/RestSdkAnnotator.java
+++ data-weave-plugin/src/main/java/org/mule/tooling/restsdk/annotator/RestSdkAnnotator.java
@@ -1,28 +1,35 @@
 package org.mule.tooling.restsdk.annotator;
 
+import amf.apicontract.client.platform.model.domain.api.WebApi;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.yaml.psi.YAMLKeyValue;
+import org.jetbrains.yaml.psi.YAMLMapping;
 import org.jetbrains.yaml.psi.YAMLScalar;
 import org.mule.tooling.restsdk.utils.RestSdkHelper;
+import org.mule.tooling.restsdk.utils.RestSdkPaths;
+import org.mule.tooling.restsdk.utils.SelectionPath;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class RestSdkAnnotator implements Annotator {
-  private List<String> URL_KEY_NAMES = Arrays.asList("url", "typeSchema", "outputType");
+  final private static List<String> URL_KEY_NAMES = List.of("url", "typeSchema", "outputType");
 
   @Override
   public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-    if (RestSdkHelper.isRestSdkDescriptorFile(element.getContainingFile())) {
+    PsiFile psiFile = element.getContainingFile();
+    if (!RestSdkHelper.isRestSdkDescriptorFile(psiFile))
+      return;
+    PsiElement parent = element.getParent();
+    if (parent instanceof YAMLKeyValue) {
       if (element instanceof YAMLScalar) {
-        PsiElement parent = element.getParent();
-        if (parent instanceof YAMLKeyValue && URL_KEY_NAMES.contains(((YAMLKeyValue) parent).getKeyText())) {
-          VirtualFile virtualFile = element.getContainingFile().getVirtualFile();
+        if (URL_KEY_NAMES.contains(((YAMLKeyValue) parent).getKeyText())) {
+          VirtualFile virtualFile = psiFile.getVirtualFile();
           if (virtualFile != null) {
             String path = ((YAMLScalar) element).getTextValue();
             VirtualFile virtualFileParent = virtualFile.getParent();
@@ -33,6 +40,22 @@ public class RestSdkAnnotator implements Annotator {
               }
             }
           }
+        }
+      } else if (element == ((YAMLKeyValue) parent).getKey()) {
+        var mapping = (YAMLMapping) parent.getParent();
+        SelectionPath yamlPath = SelectionPath.pathOfYaml(mapping);
+        if(yamlPath.matches(RestSdkPaths.ENDPOINTS_METHOD_PATH)) {
+          var endpointPath = ((YAMLKeyValue)mapping.getParent().getParent().getParent()).getKeyText();
+          String httpMethod = ((YAMLKeyValue) parent).getKeyText();
+          var webApiDocument = RestSdkHelper.parseWebApi(psiFile);
+          if (webApiDocument == null)
+            return;
+          WebApi webApi = (WebApi) webApiDocument.encodes();
+          var endpoint = RestSdkHelper.endpointByPath(webApi, endpointPath);
+          if (endpoint != null && endpoint.operations().stream().map(o -> o.method().value()).noneMatch(m -> m.equals(httpMethod)))
+            holder.newAnnotation(HighlightSeverity.ERROR, "There no " + httpMethod + " method in endpoint " + endpointPath)
+                    .range(element)
+                    .create();
         }
       }
     }
