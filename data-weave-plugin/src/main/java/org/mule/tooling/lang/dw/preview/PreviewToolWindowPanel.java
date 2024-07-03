0d03c443bd05465fdb99b78d2d613d78bfedc291
abiharbani <abiharbani@gmail.com>
2024-07-03 10:28:26 -0700

    Safe re-creation of weavePreviewComponent
    Perform an isDisposed check before recreating weavePreviewComponent

--- data-weave-plugin/src/main/java/org/mule/tooling/lang/dw/preview/PreviewToolWindowPanel.java
+++ data-weave-plugin/src/main/java/org/mule/tooling/lang/dw/preview/PreviewToolWindowPanel.java
@@ -150,7 +150,9 @@ public class PreviewToolWindowPanel extends SimpleToolWindowPanel implements Dis
           mainPanel.remove(previewComponent);
           previewComponent = null;
         }
-        weavePreviewComponent = new WeavePreviewComponent(myProject);
+        if (weavePreviewComponent.isDisposed()) {
+          weavePreviewComponent = new WeavePreviewComponent(myProject);
+        }
         previewComponent = weavePreviewComponent.createComponent();
         mainPanel.add(previewComponent, PREVIEW_EDITOR);
         weavePreviewComponent.open(psiFile);
